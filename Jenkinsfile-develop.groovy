e96a398750a0894db8841370253e288e49eafa6e
Bohdan.Liakhovetskyi <Bohdan.Liakhovetskyi@hrs.com>
2020-08-13 14:25:47 +0300

    CI dockerfile build

--- Jenkinsfile-develop.groovy
+++ Jenkinsfile-develop.groovy
@@ -4,6 +4,17 @@ pipeline {
     agent any
 
     stages {
+        stage('Build app') {
+            agent {
+                dockerfile {
+                    filename "ci/Dockerfile.build"
+                }
+            }
+
+            steps {
+                echo 'Build success'
+            }
+        }
         stage('Build & Test app') {
             steps {
                 echo "Start building application"
