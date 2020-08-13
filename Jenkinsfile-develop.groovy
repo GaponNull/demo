ea209afd06a1019d28aa86061e1c0b6b2b259675
Bohdan.Liakhovetskyi <Bohdan.Liakhovetskyi@hrs.com>
2020-08-13 15:51:14 +0300

    jenkins common build with java 8

--- Jenkinsfile-develop.groovy
+++ Jenkinsfile-develop.groovy
@@ -4,25 +4,9 @@ pipeline {
     agent any
 
     stages {
-        stage('clean_workspace_and_checkout_source') {
-            steps {
-                sh "docker-machine rm default"
-            }
-        }
         stage('Build app') {
-            agent {
-                dockerfile {
-                    filename "ci/Dockerfile.build"
-                }
-            }
-
             steps {
-                echo 'Build success'
-            }
-        }
-        stage('Build & Test app') {
-            steps {
-                echo "Start building application"
+                print "Start building application"
                 sh './gradlew clean build'
             }
         }
