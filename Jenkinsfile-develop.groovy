d43c2ee08d814d4d71fb255bf4c5a1a12de09fc4
Bohdan.Liakhovetskyi <itgaponec@gmail.com>
2020-08-21 14:32:41 +0300

    try to push docker image through registry

--- Jenkinsfile-develop.groovy
+++ Jenkinsfile-develop.groovy
@@ -32,16 +32,22 @@ pipeline {
 
         stage('Build') {
             steps {
-                sh 'aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
-                sh 'docker build -t hrs-configuration-service --network=host . '
+                sh 'docker build -t hrs-configuration-service .'
+
+                script {
+                    docker.withRegistry("912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest") {
+                        dockerImage.push()
+                    }
+                }
+//                sh 'aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
             }
         }
 
-        stage('Push to ECR') {
-            steps {
-                sh 'docker tag hrs-configuration-service:latest 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
-                sh 'docker push 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
-            }
-        }
+//        stage('Push to ECR') {
+//            steps {
+//                sh 'docker tag hrs-configuration-service:latest 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
+///               sh 'docker push 912601565515.dkr.ecr.eu-central-1.amazonaws.com/hrs-configuration-service:latest'
+//            }
+//        }
     }
 }
